b1799d4019bf953923bd76e1ddda87648b6743b8
Johanna Geßner <johanna-gessner@web.de>
2023-08-30 14:46:21 +0200

    some examples of backend validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
 
 const User = require("../models/User.model");
@@ -19,6 +20,19 @@ router.post("/signup", (req, res, next) => {
 
     const { email, password } = req.body;
 
+    // validation: required fields
+    if (!email || !password) {
+        res.status(400).render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+        return;
+    }   
+
+    // validation: pw strength
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+        res.status(400).render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+        return;
+    }
+
     bcryptjs.genSalt(saltRounds)
         .then(salt => bcryptjs.hash(password, salt))
         .then(hash => {
@@ -33,11 +47,16 @@ router.post("/signup", (req, res, next) => {
             //account created successfully
             res.redirect("/user-profile")
         })
-        .catch(error => {
+        .catch( error => {
             console.log("error creating user account... ", error);
-            next(error);
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(400).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000) {
+                res.status(400).render('auth/signup', { errorMessage: "Validation error. Email needs to be unique" });
+            } else {
+                next(error);
+            }
         })
-
 })
 
 router.get("/user-profile", (req, res, next) => {
